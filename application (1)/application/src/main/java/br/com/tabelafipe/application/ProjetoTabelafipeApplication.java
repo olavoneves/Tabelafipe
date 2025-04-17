52a8638932b5b36526f5ef87d397c2ea33e485ae
Olavo Neves <olavo9neves@gmail.com>
2025-04-17 16:30:26 -0300

    Adiciona spring

---
+++ application (1)/application/src/main/java/br/com/tabelafipe/application/ProjetoTabelafipeApplication.java
@@ -0,0 +1,13 @@
+package br.com.tabelafipe.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTabelafipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTabelafipeApplication.class, args);
+	}
+
+}
---
+++ application (1)/application/src/test/java/br/com/tabelafipe/application/ProjetoTabelafipeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tabelafipe.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoTabelafipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
